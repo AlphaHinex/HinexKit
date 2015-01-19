4279a7e306eb6bc380164536725db08d3a484686
AlphaHinex <AlphaHinex@gmail.com>
2015-01-19 23:56:02 +0800

    could batch download

--- singer_s3/python2/singer_s3.py
+++ singer_s3/python2/singer_s3.py
@@ -1,11 +1,13 @@
 import urllib2
 
-sid    =   136913
-margin =  30000000
+sids = [101824312, 5016169, 101824306, 465340, 101824313, 2530780]
+margin = 30000000
+path = '/Users/alphahinex/Desktop'
 
-req = urllib2.Request("http://115.231.20.59:80/%d.mp3" %(sid+margin))
-req.add_header('Host', 'stream19.qqmusic.qq.com')
-req.add_header('Cookie', 'qqmusic_uin=12345678; qqmusic_key=12345678; qqmusic_fromtag=30')
-response = urllib2.urlopen(req)
-by = response.read()
-open("/Users/alphahinex/Desktop/%d.mp3" %(sid+margin), "wb").write(by)
+for sid in sids:
+	req = urllib2.Request("http://115.231.20.59:80/%d.mp3" %(sid+margin))
+	req.add_header('Host', 'stream19.qqmusic.qq.com')
+	req.add_header('Cookie', 'qqmusic_uin=12345678; qqmusic_key=12345678; qqmusic_fromtag=30')
+	response = urllib2.urlopen(req)
+	by = response.read()
+	open("%s/%d.mp3" %(path, sid+margin), "wb").write(by)
