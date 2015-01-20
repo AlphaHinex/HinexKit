ac2625696c256acc98ed1407b7c6871fe14ec9e1
AlphaHinex <AlphaHinex@gmail.com>
2015-01-20 23:33:00 +0800

    only need to input date

--- singer_s3/python2/singer_s3.py
+++ singer_s3/python2/singer_s3.py
@@ -1,13 +1,23 @@
 import urllib2
+import json
 
-sids = [101824312, 5016169, 101824306, 465340, 101824313, 2530780]
-margin = 30000000
+date = '20150102'
 path = '/Users/alphahinex/Desktop'
+margin = 30000000
 
-for sid in sids:
-	req = urllib2.Request("http://115.231.20.59:80/%d.mp3" %(sid+margin))
-	req.add_header('Host', 'stream19.qqmusic.qq.com')
-	req.add_header('Cookie', 'qqmusic_uin=12345678; qqmusic_key=12345678; qqmusic_fromtag=30')
-	response = urllib2.urlopen(req)
-	by = response.read()
-	open("%s/%d.mp3" %(path, sid+margin), "wb").write(by)
+req = urllib2.Request("http://y.qq.com/m/act/iamsinger3/%s.json" %date)
+response = urllib2.urlopen(req)
+resp_str = response.read()
+json_obj = json.loads(resp_str[15:-1])
+
+for obj in json_obj:
+	sids = [obj['track']['id'], obj['original_track']['id']]
+	for sid in sids:
+		req = urllib2.Request("http://115.231.20.59:80/%d.mp3" %(sid+margin))
+		req.add_header('Host', 'stream19.qqmusic.qq.com')
+		req.add_header('Cookie', 'qqmusic_uin=12345678; qqmusic_key=12345678; qqmusic_fromtag=30')
+		response = urllib2.urlopen(req)
+		print "prepare to download %d ..." %(sid+margin)
+		by = response.read()
+		open("%s/%d.mp3" %(path, sid+margin), "wb").write(by)
+		print "download %d done" %(sid+margin)
